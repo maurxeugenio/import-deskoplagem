68788d12f2335e5ab246082b5217f344930c2d97
Mauricio Eugênio <mauricio@digitalbluemarketing.com.br>
2019-03-07 18:46:19 -0300

    deskoplagem - open and close aplication time

--- desklopagem.js
+++ desklopagem.js
@@ -1,43 +1,60 @@
+function showDeskoplagem(openTime, closingTime){
+    var spTime = new Date().toLocaleString("pt-BR", {timeZone: "America/Sao_Paulo"});
+    spTime = new Date(spTime);
+    var hours = spTime.getHours();
 
-function get_deskoplagem(args){
-    $('html').append(`<iframe id="desklopagemIframe" src="https://deskoplagem.dbdesk.com.br/${args['hash']}/" width="400" height="600"></iframe>
-    <button id="desklopagemBTN" type="button" onclick="show()" name="button">${args['text'] ? args['text']: 'Auto atendimento'}</button>
-
-    <style media="screen">
-    #desklopagemIframe{
-        float: right;
-        position: absolute;
-        bottom: 12%;
-        right: 5%;
-        overflow: hidden;
-        border: none;
-        display: none;
-        border-radius: 2px;
-        box-shadow: 2px 2px 10px rgba(0,0,0,.5);
-
+    if (hours > openTime || hours < closingTime ){
+        console.log(spTime);
+        return true;
     }
     
-    #desklopagemBTN{
-        min-width: 200px;
-        width:auto;
-        background-color: ${ args['btnBG'] ? args['btnBG'] : '#000'};
-        color: ${args['btnColor'] ? args['btnColor'] : 'white'};
-        height: 60px;
-        position: absolute;
-        float: right;
-        right: 5%;
-        bottom: 4%;
-        border-radius: 5px;
-        border: none;
-        overflow: hidden;
-        font-size: 14pt;
-        box-shadow: 2px 2px 5px rgba(0, 0,0, .6);
+    console.log(spTime);
+    return false;
+}
+
+
+function get_deskoplagem(args){
+
+    if (showDeskoplagem(args['openTime'], args['closingTime']) !== false){
+        $('html').append(`<iframe id="desklopagemIframe" src="https://deskoplagem.dbdesk.com.br/${args['hash']}/" width="400" height="600"></iframe>
+        <button id="desklopagemBTN" type="button" onclick="show()" name="button">${args['text'] ? args['text']: 'Auto atendimento'}</button>
+
+        <style media="screen">
+        #desklopagemIframe{
+            float: right;
+            position: absolute;
+            bottom: 12%;
+            right: 5%;
+            overflow: hidden;
+            border: none;
+            display: none;
+            border-radius: 2px;
+            box-shadow: 2px 2px 10px rgba(0,0,0,.5);
+
+        }
+
+        #desklopagemBTN{
+            min-width: 200px;
+            width:auto;
+            background-color: ${ args['btnBG'] ? args['btnBG'] : '#000'};
+            color: ${args['btnColor'] ? args['btnColor'] : 'white'};
+            height: 60px;
+            position: absolute;
+            float: right;
+            right: 5%;
+            bottom: 4%;
+            border-radius: 5px;
+            border: none;
+            overflow: hidden;
+            font-size: 14pt;
+            box-shadow: 2px 2px 5px rgba(0, 0,0, .6);
+        }
+        button:hover{
+            box-shadow: 2px 2px 15px rgba(0, 0,0, .8);
+        }
+        </style>
+        `)
     }
-    button:hover{
-        box-shadow: 2px 2px 15px rgba(0, 0,0, .8);
-    }
-    </style>
-    `)
 }
 
 function show(){
--- readme.md
+++ readme.md
@@ -10,22 +10,25 @@ Deskoplagem é uma aplicação em Vue Js que consulta a base de dados do DBDESK 
 
 
 ``` js
-<script
+    <script
     src="https://code.jquery.com/jquery-3.3.1.min.js"
     integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
     crossorigin="anonymous">
-</script>
+    </script>
 
-<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/maurxeugenio/import-deskoplagem/desklopagem.js"></script>
+    <script type="text/javascript" src="desklopagem.js"></script>
 
-<script type="text/javascript">
+    <script type="text/javascript">
     get_deskoplagem({
-        'hash': 'f31231dasd3dsqweqw', // Hash do curso
+        'hash': '6827e7f3ea86799d0efa2aaaee543211', // Hash do curso
         'text': 'Tirar dúvidas', // titulo botao
         'btnBG':  '#5b8cdb', // Cor do botao
-        'btnColor': 'white' // Cor da fonte
-        }
+        'btnColor': 'white', // Cor da fonte
+        'openTime': 22, // hora que começa aparecer
+        'closingTime': 08, // hora que para de aparecer
+
+    }
     )
-</script>
+    </script>
 
 ```
